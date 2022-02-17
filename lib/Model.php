67f97f6be68cdd9ccfce3d750262ddd561f45a88
Rick <t83zxcv@gmail.com>
2022-02-17 19:35:23 +0800

    model

--- lib/Model.php
+++ lib/Model.php
@@ -12,7 +12,7 @@
             
         }
 
-        public function Create($data){
+        public function create($data){
 
             foreach($data as $k=>$v){
                 $data[$k]=$this->db->replacedata($v);
@@ -51,17 +51,17 @@
             return $this;
         }
 
-        public function Desc($desc){
+        public function desc($desc){
             $this->sql_str.=' ORDER BY `'.$desc.'` DESC';
             return $this;
         }
 
-        public function Limit($LIMIT){
+        public function limit($LIMIT){
             $this->sql_str.=' LIMIT '.$LIMIT;
             return $this;
         }
 
-        public function Delete($id,$debug=false){
+        public function delete($id,$debug=false){
             $id=$this->db->replacedata($id);
             $sql="DELETE FROM `".$this->table."` where " .$this->primaryKey."='".$id."' ".$this->sql_str;
             if($debug){
@@ -84,7 +84,7 @@
             return $this;
         }
 
-        public function Update($id='',$data=array(),$debug=false){
+        public function update($id='',$data=array(),$debug=false){
 
             foreach($data as $k=>$v){
                 $data[$k]=$this->db->replacedata($v);
@@ -103,7 +103,7 @@
              return $this->db->sqlquery($sql);
         }
 
-        public function SelectData($data=array()){
+        public function selectData($data=array()){
 
             $sql='Select * From `'.$this->table.'` where 1 '.$this->sql_str;
             $obj=$this->db->sqlquery($sql);
